7fb1bdd076e424c2d0b3e1be6ea13f4fffa863c7
TiTrist <thoravaltristan@gmail.com>
2019-01-22 18:25:22 +0100

    Ajout Tristan Galerie

---
+++ html/src/main_galerie.php
@@ -0,0 +1,56 @@
+           <section class="galery flex_wrapper">
+                <article class="image">
+                    <a href="#" class="">
+                        <img src="images/produit-1.jpg" alt="">
+                        <div class="doc_information">
+                            <h3 class="doc_title">Des chats</h3>
+                            <img class="doc_type" src="images/burger-icon.png" alt="">
+                        </div>
+                        <input type="checkbox" class="select">
+                    </a>
+                </article>
+                <article class="image">
+                    <a href="#" class="">
+                        <img src="images/produit-2.jpg" alt="">
+                        <div class="doc_information">
+                            <h3 class="doc_title">Des animaux</h3>
+                            <img class="doc_type" src="images/burger-icon.png" alt="">
+                        </div>
+                        <input type="checkbox" class="select">
+                    </a>
+                </article>
+                <article class="image">
+                    <a href="#" class="">
+                        <img src="images/produit-2.jpg" alt="">
+                        <div class="doc_information">
+                            <h3 class="doc_title">Des animaux</h3>
+                            <img class="doc_type" src="images/burger-icon.png" alt="">
+                        </div>
+                        <input type="checkbox" class="select">
+                    </a>
+                </article>
+                <article class="image">
+                    <a href="#" class="">
+                        <img src="images/produit-2.jpg" alt="">
+                        <div class="doc_information">
+                            <h3 class="doc_title">Des animaux</h3>
+                            <img class="doc_type" src="images/burger-icon.png" alt="">
+                        </div>
+                        <input type="checkbox" class="select">
+                    </a>
+                </article>
+                <article class="image">
+                    <a href="#" class="">
+                        <img src="images/produit-2.jpg" alt="">
+                        <div class="doc_information">
+                            <h3 class="doc_title">Des animaux</h3>
+                            <img class="doc_type" src="images/burger-icon.png" alt="">
+                        </div>
+                    </a>
+                    <div class="checkbox select">
+                        <input type="checkbox">
+                        <label for="checkbox"></label>
+                    </div>
+                </article>
+                
+            </section>
