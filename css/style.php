c807bb8ad430a94a27cc9887bcb97266e0eaa661
kbleneau <kevin.bleneau@etudiant.univ-lr.fr>
2019-01-29 11:30:41 +0100

    css fixed

---
+++ css/style.php
@@ -0,0 +1,10 @@
+<?php
+    header("Content-type: text/css; charset: UTF-8");
+
+    include 'header.css';
+    include 'searchbar.css';
+    include 'nav.css';
+    include 'main_btn.css';
+    include 'main_galerie.css';
+    include 'footer.css';
+?>
--- html/src/main_btn.php
+++ html/src/main_btn.php
@@ -1,18 +1,18 @@
 <section class="main_btn">
-<a href="#" id="diaporama">
-Diaporama
-<i class="fas fa-play-circle fa-lg"></i></a>
+	<a href="#" id="diaporama">
+	Diaporama
+	<i class="fas fa-play-circle fa-lg"></i></a>
 
-<a href="#" id="ajouter">
-Ajouter
-<i class="fas fa-plus-circle fa-lg"></i></a>
+	<a href="#" id="ajouter">
+	Ajouter
+	<i class="fas fa-plus-circle fa-lg"></i></a>
 
-<a href="#" id="deplacer">
-Déplacer
-<i class="fas fa-arrow-circle-right fa-lg"></i></a>
+	<a href="#" id="deplacer">
+	Déplacer
+	<i class="fas fa-arrow-circle-right fa-lg"></i></a>
 
-<a href="#" id="supprimer">
-Supprimer
-<i class="fas fa-trash-alt"></i></a>
+	<a href="#" id="supprimer">
+	Supprimer
+	<i class="fas fa-trash-alt"></i></a>
 
 </section>
