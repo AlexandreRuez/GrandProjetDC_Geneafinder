7512e3dfcb5be421c1257053165ba1df7d9bb7f0
darkfeaver <darkefeaver@gmail.com>
2019-01-29 18:30:08 +0100

    section footer

--- html/src/footer.php
+++ html/src/footer.php
@@ -1,6 +1,6 @@
 <footer>
 		<section id="social">
-			<h4>SUIVEZ NOUS !</h4>
+			<h4>SUIVEZ NOUS SUR LES RÉSEAUX !</h4>
 			<div id="reseau">
 				<a href="https://www.facebook.com/Geneafinder-541996719522534/?ref=br_rs" id="facebook">facebook</a>
 				<a href="https://twitter.com/geneafinder?lang=fr" id="twitter">twitter</a>
@@ -8,11 +8,7 @@
 		</section>
 			
 
-		<div class="copyright">
-			<p>Généafinder
-			© 2019 Doptim </p>
-            <address id="address">3 ALL DES BARDES - 35760 SAINT GREGOIRE</address>
-		</div>
+
 		
 
 		
