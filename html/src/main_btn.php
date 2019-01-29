6f4152befd009799a4ee26785b6b2f428d5b7067
Lolo <Lolo@MacBook-Pro-de-Laure-Anne-2.local>
2019-01-29 19:00:34 +0100

    js boutons

--- html/src/main_btn.php
+++ html/src/main_btn.php
@@ -3,6 +3,8 @@
 	Diaporama
 	<i class="fas fa-play-circle fa-lg"></i></a>
 
+	<button type="button" id="image">Image</button>
+
 	<a href="#" id="ajouter">
 	Ajouter
 	<i class="fas fa-plus-circle fa-lg"></i></a>
@@ -16,3 +18,18 @@
 	<i class="fas fa-trash-alt"></i></a>
 
 </section>
+
+<script>
+
+var checkbox = document.getElementById('image');
+var btn_deplacer = document.getElementById('deplacer');
+var btn_supprimer = document.getElementById('supprimer');
+var btn_ajouter = document.getElementById('ajouter');
+
+checkbox.addEventListener('click', function() {
+	btn_deplacer.style.display = "block";
+	btn_supprimer.style.display = "block";
+	btn_ajouter.style.marginRight = "10px";
+})
+
+</script>
