e546107fedd55900a0a361d5ce89309dd62c617d
kbleneau <kevin.bleneau@etudiant.univ-lr.fr>
2019-01-29 19:08:44 +0100

    header sans css

--- html/src/header.php
+++ html/src/header.php
@@ -1,3 +1,16 @@
 <header>
-	<h1>Geneafinder</h1>
+	<nav class="nav_header">
+		<ul class="container_nav_header">
+			<li id="li-g"><a href=""><img src="../../../img/geneafinder-logo_nav.png"> G</a></li>
+			<li id="li-eneafinder"><a href=""><h1>Geneafinder</h1></a></li>
+			<li id="li-tutos"><a href="">tutos</a></li>
+			<li id="li-blog"><a href="">blog</a></li>
+			<li id="li-facebook"><a href="">Facebook</a></li>
+			<li id="li-twitter"><a href="">Twitter</a></li>
+			<li id="li-settings"><a href="">Settings</a></li>
+			<li id="li-deconnexion"><a href="">Déconnexion</a></li>
+			<li id="li-infos"><a href="">infos</a></li>
+			<li id="li-aide"><a href="">aide</a></li>
+		</ul>
+	</nav>
 </header>
