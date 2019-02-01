9eac570c3a2323fefd4c564ccf220ee4da2ca302
Laure-Anne <laureanne.leneel@gmail.com>
2019-02-01 13:24:54 +0100

    Modification positionnement header + fin galerie

--- html/src/main_galerie.php
+++ html/src/main_galerie.php
@@ -4,19 +4,59 @@
                         <img src="../img/produit-1.jpg" alt="">
                         <div class="doc_information">
                             <h3 class="doc_title">Des chats</h3>
-                            <img class="doc_type" src="../img/burger-icon.png" alt="">
+                            <i class="fas fa-image fa-lg"></i>
                         </div>
-                        <input type="checkbox" class="select">
                     </a>
+                    <label for="image-1"></label>
+                    <input type="checkbox" id="image-1" class="select">
                 </article>
+               
                 <article class="image">
                     <a href="#" class="">
                         <img src="../img/produit-2.jpg" alt="">
                         <div class="doc_information">
                             <h3 class="doc_title">Des animaux</h3>
-                            <img class="doc_type" src="../img/burger-icon.png" alt="">
+                            <i class="fas fa-video fa-lg"></i>
                         </div>
-                        <input type="checkbox" class="select">
                     </a>
+                    <label for="image-2"></label>
+                    <input type="checkbox" id="image-2" class="select">
                 </article>
+               
+               <article class="image">
+                    <a href="#" class="">
+                        <img src="../img/produit-2.jpg" alt="">
+                        <div class="doc_information">
+                            <h3 class="doc_title">Des animaux</h3>
+                            <i class="fas fa-video fa-lg"></i>
+                        </div>
+                    </a>
+                    <label for="image-3"></label>
+                    <input type="checkbox" id="image-3" class="select">
+                </article>
+               
+               <article class="image">
+                    <a href="#" class="">
+                        <img src="../img/produit-2.jpg" alt="">
+                        <div class="doc_information">
+                            <h3 class="doc_title">Des animaux</h3>
+                            <i class="fas fa-video fa-lg"></i>
+                        </div>
+                    </a>
+                    <label for="image-4"></label>
+                    <input type="checkbox" id="image-4" class="select">
+                </article>
+               
+               <article class="image">
+                    <a href="#" class="">
+                        <img src="../img/produit-2.jpg" alt="">
+                        <div class="doc_information">
+                            <h3 class="doc_title">Des animaux</h3>
+                            <i class="fas fa-file-alt fa-lg"></i>
+                        </div>
+                    </a>
+                    <label for="image-4"></label>
+                    <input type="checkbox" id="image-4" class="select">
+                </article>
+           
             </section>
