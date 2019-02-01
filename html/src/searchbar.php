4d958146ad49e888e9c4e8db61b8dc6436bbda8a
Laure-Anne <laureanne.leneel@gmail.com>
2019-02-01 14:05:23 +0100

    Correction searchbar

--- html/src/searchbar.php
+++ html/src/searchbar.php
@@ -2,7 +2,9 @@
     <div class="mon-cercle">
         <p class="stk">Stockage</p>
     </div>
+    <div class="search">
     <input type="search" class="site-search" name="q" aria-label="Search through site content" placeholder="Saisissez votre recherche">
-    <button type="button" name="search" id="btn"><i class="fas fa-search fa-3x"></i></button>
-    <button type="button" name="delet" id="btn"><i class="far fa-times-circle fa-3x"></i></button>
+    <button type="button" name="search"><i class="fas fa-search fa-2x"></i></button>
+    <button type="button" name="delete" id="btn"><i class="far fa-times-circle fa-2x"></i></button>
+    </div>
 </section>
