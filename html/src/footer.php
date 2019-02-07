7c0abd1093c628ca20c2ce5d80ec8eb2465455e6
darkfeaver <darkefeaver@gmail.com>
2019-02-07 17:06:05 +0100

    section footer reseaux sociaux blankpage

--- html/src/footer.php
+++ html/src/footer.php
@@ -7,8 +7,8 @@
 		<section id="social">
 			<h4>Suivez-nous sur les réseaux sociaux !</h4>
 			<div id="reseau">
-				<a href="https://www.facebook.com/Geneafinder-541996719522534/?ref=br_rs" id="facebook">facebook</a>
-				<a href="https://twitter.com/geneafinder?lang=fr" id="twitter">twitter</a>
+				<a href="https://www.facebook.com/Geneafinder-541996719522534/?ref=br_rs" target="_blank" id="facebook">facebook</a>
+				<a href="https://twitter.com/geneafinder?lang=fr"  target="_blank" id="twitter">twitter</a>
 			</div>
 		</section>
 
