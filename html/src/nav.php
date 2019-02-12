28db7d1d553a91f2fbae90da51ee207945f3babd
AlexandreRuez <37704226+AlexandreRuez@users.noreply.github.com>
2019-02-12 18:13:36 +0100

    Update nav.php

--- html/src/nav.php
+++ html/src/nav.php
@@ -1,14 +1,34 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Galerie</title>
+    <link rel="stylesheet" href="../css/main_nav.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
+<body>
+<header>
+    <p>Header GENEAFINDER</p>
+</header>
+<hr>
+<section class="searchbar">
+    <p>Barre de recherche + stockage</p>
+</section>
+<hr>
 <nav id="Album_wrapper">
     <div id="Album_left">  </div>
     <ul id="Album_list">
         <li id="Album_add"> <i class="fas fa-plus-circle fa-5x"></i> </li>
-        <li id="" class="navitem"> <p> Jean René 1974 </p> </li>
-        <li id="navitem_1" class="navitem"> <p> Arrières grand parents </p> </li>
-        <li id="navitem_2" class="navitem"> <p> Colette 1985 </p> </li>
-        <li id="navitem_3" class="navitem"> <p> Vacances jacques 1960 </p> </li>
-        <li id="navitem_4" class="navitem"> <p> Ski 1987 </p> </li>
-        <li id="" class="navitem"> <p> Potos école </p> </li>
-        <li id="" class="navitem"> <p> Photos vacances </p> </li>
+        <li id="" class="navitem"> <div style='background-image: url("../img/photo-1.jpg");'> </div> <p> Jean René 1974 </p> </li>
+        <li id="navitem_1" class="navitem"> <div style="background-image: url('../img/photo-2.jpg')"> </div> <p> Arrières grand parents </p> </li>
+        <li id="navitem_2" class="navitem"> <div style="background-image: url('../img/photo-3.jpg')"> </div> <p> Colette 1985 </p> </li>
+        <li id="navitem_3" class="navitem"> <div style="background-image: url('../img/photo-4.jpg')"> </div> <p> Vacances jacques 1960 </p> </li>
+        <li id="navitem_4" class="navitem"> <div style="background-image: url('../img/photo-5.jpg')"> </div> <p> Ski 1987 </p> </li>
+        <li id="" class="navitem"> <div style="background-image: url('../img/photo-6.jpg')"> </div> <p> Photos école </p> </li>
+        <li id="" class="navitem"> <div style="background-image: url('../img/photo-7.jpg')"> </div> <p> Photos vacances </p> </li>
     </ul>
     <div id="Album_right"> </div>
 </nav>
@@ -63,3 +83,15 @@
     });
 
 </script>
+
+<hr>
+<section class=main>
+    <p>Main = galerie</p>
+</section>
+<hr>
+<footer>
+    <p>Footer GENEAFINDER</p>
+</footer>
+
+</body>
+</html>
