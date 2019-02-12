273cbedf036d018b7ac53c530ad5bd3296cb5f55
Laure-Anne <laureanne.leneel@gmail.com>
2019-02-12 18:56:40 +0100

    pop-up déplacer

---
+++ html/src/popup_deplacer.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" type="text/css" href="../../css/popup_deplacer.css">
+
+<div class="popup_deplacer">
+    <form>
+        <label for="album">Album</label>
+        <select name="album" id="album">
+            <option value="jean-rene-1974">Jean René 1974</option>
+            <option value="arrieres-grands-parents">Arrières grand-parents</option>
+            <option value="colette-1985">Colette 1985</option>
+            <option value="vacances-jacques-1960">Vacances Jacques 1960</option>
+        </select>
+
+        <input type="submit" value="Déplacer">
+    </form>
+</div>
