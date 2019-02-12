1f84e979214ce4931e62745876cef78167101d62
TiTrist <thoravaltristan@gmail.com>
2019-02-12 19:01:09 +0100

    apparition bouton

--- html/src/footer.php
+++ html/src/footer.php
@@ -1,28 +1,56 @@
 <a href="#top-page" id="btn_top-page"><span>Vers le haut de la page</span><i class="fas fa-angle-up fa-lg"></i></a>
-
+<script>
+    var checkboxs = document.querySelectorAll('input[type="checkbox"]');
+    var btn_ajouter = document.getElementById('ajouter');
+    var btn_deplacer = document.getElementById('deplacer');
+    var btn_supprimer = document.getElementById('supprimer');
+    var count = 0;
+    
+    checkboxs.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+        if (elem.checked === true)
+        {   
+                count++;
+        } else {
+                count --;
+        }
+        if (count > 0) 
+        {   
+                btn_deplacer.style.display = "block";
+                btn_supprimer.style.display = "block";
+        } else {
+                btn_deplacer.style.display = "none";
+                btn_supprimer.style.display = "none";
+        }
+    ;
+    })
+});   
+    
+    
+</script>
 <footer>
-    
-    <hr>
-    
-		<section id="social">
-			<h4>Suivez-nous sur les réseaux sociaux !</h4>
-			<div id="reseau">
-				<a href="https://www.facebook.com/Geneafinder-541996719522534/?ref=br_rs" target="_blank" id="facebook">facebook</a>
-				<a href="https://twitter.com/geneafinder?lang=fr"  target="_blank" id="twitter">twitter</a>
-			</div>
-		</section>
 
-	</footer>
+    <hr>
+
+    <section id="social">
+        <h4>Suivez-nous sur les réseaux sociaux !</h4>
+        <div id="reseau">
+            <a href="https://www.facebook.com/Geneafinder-541996719522534/?ref=br_rs" target="_blank" id="facebook">facebook</a>
+            <a href="https://twitter.com/geneafinder?lang=fr"  target="_blank" id="twitter">twitter</a>
+        </div>
+    </section>
+
+</footer>
 
 <script>
-    
+
     var toppage = document.getElementById("btn_top-page");
-    
-    window.addEventListener("scroll", function() {
-    if (pageYOffset > 100) {
-        toppage.style.display = "block"
-    } else {
-        toppage.style.display = "none";
-    }})
-    
-    </script>
+    window.addEventListener("scroll", function () {
+        if (pageYOffset > 100) {
+            toppage.style.display = "block"
+        } else {
+            toppage.style.display = "none";
+        }
+    })
+
+</script>
--- html/src/main_btn.php
+++ html/src/main_btn.php
@@ -1,41 +1,18 @@
 <section class="main_btn">
-	<a href="#" id="diaporama">
-	Diaporama
-	<i class="fas fa-play-circle fa-lg"></i></a>
+    <a href="#" id="diaporama">
+        Diaporama
+        <i class="fas fa-play-circle fa-lg"></i></a>
+        
+    <a href="#" id="ajouter">
+        Ajouter
+        <i class="fas fa-plus-circle fa-lg"></i></a>
 
-	<input type="checkbox" id="image">
+    <a href="#" id="deplacer">
+        Déplacer
+        <i class="fas fa-arrow-circle-right fa-lg"></i></a>
 
-	<a href="#" id="ajouter">
-	Ajouter
-	<i class="fas fa-plus-circle fa-lg"></i></a>
-
-	<a href="#" id="deplacer">
-	Déplacer
-	<i class="fas fa-arrow-circle-right fa-lg"></i></a>
-
-	<a href="#" id="supprimer">
-	Supprimer
-	<i class="fas fa-trash-alt"></i></a>
+    <a href="#" id="supprimer">
+        Supprimer
+        <i class="fas fa-trash-alt"></i></a>
 
 </section>
-
-<script>
-
-
-
-var checkbox = document.getElementById('image');
-var btn_ajouter = document.getElementById('ajouter');
-var btn_deplacer = document.getElementById('deplacer');
-var btn_supprimer = document.getElementById('supprimer');
-
-checkbox.addEventListener('click', function() {
-	if (checkbox.checked) {
-	btn_deplacer.style.display = "block";
-	btn_supprimer.style.display = "block";
-} else {
-	btn_deplacer.style.display = "none";
-	btn_supprimer.style.display = "none";
-}
-})
-
-</script>
