00c441ae35fa6c6513250ec6d4ba9bb1c589acc2
darkfeaver <darkefeaver@gmail.com>
2019-02-25 16:00:54 +0100

    bouton suppression html et css

---
+++ html/src/popup_supprimer.php
@@ -0,0 +1,45 @@
+<!doctype html>
+
+<html lang="fr">
+
+<title>bouton Supprimer</title>
+
+
+<link rel="popup_supprimer" href="">
+
+</head>
+
+
+<body>
+
+
+<form action="#" id="#" method="POST">
+
+
+    <input type="radio" name="delete" value="deplace" checked>
+    <label for="marrons">Supprimer de l'album</label>
+    <p class="text__bottom">Les documents seront placés dans l'album par défaut</p>
+
+    <br><br>
+
+    <input type="radio" name="delete" value="definitif">
+    <label for="bleus">Supprimer défénitivement</label>
+
+    <br>
+
+    <button type="button" name="delete" class="button__delete">Supprimer</button>
+
+</form>
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
