1ac3517ac46dc9d5d2373d0f47da1993b844c0df
TiTrist <thoravaltristan@gmail.com>
2019-02-26 18:27:11 +0100

    modif js

--- html/src/footer.php
+++ html/src/footer.php
@@ -1,11 +1,11 @@
 <a href="#top-page" id="btn_top-page"><span>Vers le haut de la page</span><i class="fas fa-angle-up fa-lg"></i></a>
 <script>
-    var checkboxs = document.querySelectorAll('input[type="checkbox"]');
+    var checkboxs = document.querySelectorAll('input.input_check[type="checkbox"]');
     var btn_ajouter = document.getElementById('ajouter');
     var btn_deplacer = document.getElementById('deplacer');
     var btn_supprimer = document.getElementById('supprimer');
     var count = 0;
-    
+
     checkboxs.forEach(function (elem) {
     elem.addEventListener("click", function () {
         if (elem.checked === true)
