145d03f74069ef11c92878665719f41483bd0677
Laure-Anne <laureanne.leneel@gmail.com>
2019-03-01 10:31:24 +0100

    correction barre action position fixed

--- html/src/main_btn.php
+++ html/src/main_btn.php
@@ -18,3 +18,28 @@
 
 </section>
 
+<script>
+    
+    //barre actions
+
+    
+    var barre_action = document.getElementById("actions");
+    var galerie = document.getElementById("galerie");
+    
+    window.addEventListener("scroll", function() {
+    if (pageYOffset > 490) {
+        barre_action.style.position = "fixed";
+        barre_action.style.top = "82px";
+        barre_action.style.marginBottom = "100px";
+        barre_action.style.marginTop = "0";
+        galerie.style.paddingTop = "110px";
+        
+    } else {
+        barre_action.style.position = "relative";
+        barre_action.style.top = "auto";
+        barre_action.style.marginBottom = "20px";
+        barre_action.style.marginTop = "20px";
+        galerie.style.paddingTop = "0";
+    }});
+
+</script>
